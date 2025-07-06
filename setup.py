b14d05094bffb7a0bf4d766dea1ee5e973918bf6
Kiran Majji <kiranmajji1988@gmail.com>
2025-07-06 11:26:21 +0530

    Logging and exception

--- setup.py
+++ setup.py
@@ -2,21 +2,26 @@ from setuptools import find_packages, setup
 from typing import List
 
 
-HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->List[str]:
+HYPEN_E_DOT = '-e .'# This is a constant variable that is used to remove the -e . line from the requirements file, which is used for editable installs.
+def get_requirements(file_path:str)->List[str]: # This function will return the list of requirements from the requirements.txt file
 
     '''
     This function will return the list of requirements
     '''
     requirements=[]    
-    with open(file_path) as file_obj:
+    with open(file_path) as file_obj: # Open the requirements.txt file in read mode
+        # file_obj is a file object that we can use to read the contents of the file
+        # We are using the with statement to open the file, which will automatically close the file
+        # after the block of code is executed, even if an exception is raised.
+        # This is a good practice to avoid leaving files open and consuming system resources.
+        # We are using the readlines() method to read all the lines in the file and 
         requirements= file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("\n","")for req in requirements] # While reading the requirements file, it includes \n at the end of each line, so we need to remove it.
 
         if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            requirements.remove(HYPEN_E_DOT) # This is used to remove the -e . line from the requirements file, which is used for editable installs.
 
-        return requirements
+        return requirements # This will return the list of requirements without the -e . line
 
 setup(
 name='mlproject',
@@ -24,7 +29,7 @@ version='0.0.1',
 author= 'Kiran',
 author_email='kiranmajji1988@gmail.com',
 packages= find_packages(),
-install_requires = get_requirements('requirements.txt')
+install_requires = get_requirements('requirements.txt') # This will call the get_requirements function to get the list of requirements from the requirements.txt file
 
 )
 
---
+++ src/exception.py
@@ -0,0 +1,44 @@
+import sys # This is used to get the information about the current exception
+import logging # This is used to log the error message
+
+def error_message_detail(error,error_detail:sys): # This function will return the error message with the file name and line number where the error occurred
+    _,_,exc_tb=error_detail.exc_info() # This will get the current exception information
+    # exc_info() returns a tuple of three values: the exception type, the exception value, and the traceback object.
+    # The traceback object contains information about the call stack at the point where the exception was raised.
+    # We are using the _ to ignore the first two values of the tuple, which are not needed for our purpose.
+    # The traceback object contains information about the call stack at the point where the exception was raised
+    # We are using the exc_tb variable to get the traceback object.
+    # The traceback object contains information about the call stack at the point where the exception was raised
+    # exc_tb.tb_frame is the frame object for the current function call
+    # exc_tb.tb_frame.f_code is the code object for the current function call
+    # exc_tb.tb_frame.f_code.co_filename is the name of the file where the current function call was made
+    # exc_tb.tb_lineno is the line number where the current function call was made
+             
+    file_name=exc_tb.tb_frame.f_code.co_filename # This will get the name of the file where the error occurred
+    # exc_tb.tb_frame.f_code.co_filename is the name of the file where the current function call was made
+    # exc_tb.tb_lineno is the line number where the current function call
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    file_name, exc_tb.tb_lineno,str(error) # This will get the error message
+
+    )
+    return error_message # This will return the error message with the file name and line number where the error occurred
+    
+
+class CustomeException(Exception): # This is a custom exception class that inherits from the built-in Exception class
+        """Custom Exception class for handling exceptions in the application."""
+        def __init__(self, error_message, error_detail:sys): # This is the constructor method that initializes the custom exception with an error message and error details
+            super().__init__(error_message) # This will call the constructor of the parent class Exception 
+            self.error_message = error_message_detail(error_message, error_detail = error_detail) # This will get the error message with the file name and line number where the error occurred
+
+        def __str__(self): # This is the string representation of the custom exception 
+            return self.error_message # This will return the error message with the file name and line number where the error occurred
+            
+#if __name__=="__main__": # This is the main function that will be executed when the script is run
+    #try:
+        #a = 1 / 0 # This will raise a ZeroDivisionError
+    #except Exception as e: # This will catch the exception and print the error message
+        #raise CustomeException(e, sys) # This will raise the custom exception with the error message and error details
+
+    
+
+
